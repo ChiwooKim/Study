035c25f45ade1e9da6d7baa17c151c97b8471b25
kcw0360 <kcw0360@naver.com>
2021-12-19 22:53:29 +0900

    새샘이의 7-3-5 게임

---
+++ Algorithm/SWEA/D3/5948_새샘이의_7-3-5_게임/s1.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('input.txt')
+
+from itertools import combinations
+
+for tc in range(int(input())):
+    numbers = list(map(int, input().split()))
+    result = set()
+    # 모든 경우의 수를 구한 후 합한 값들을 결과에 담는다(set으로 중복제거)
+    for i in combinations(numbers, 3):
+        result.add(sum(i))
+    # 큰 수 에서 5번째를 뽑기 위해 set을 list로 바꾼 후 정렬
+    result = sorted(list(result))
+
+    print('#{} {}'.format(tc+1, result[-5]))
+
